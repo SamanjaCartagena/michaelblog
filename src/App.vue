13ae03e834f1483520938baade2d854764500e2c
Samanja Cartagena <c.samanja09@Pabs.local>
2023-09-13 18:15:07 -0400

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header-page></header-page>
+  </div>
+</template>
+
+<script>
+import HeaderPage from './components/HeaderPage.vue'
+
+export default {
+  components:{
+    HeaderPage
+  }
+
+}
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+
+</style>
